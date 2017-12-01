2fd5cbc8549a34d7c6534b386a39effe2611d2d1
maximilianone <maximilianone22@gmail.com>
2017-12-01 23:28:39 +0200

    Add files via upload

---
+++ task61/controller/FileController.java
@@ -0,0 +1,48 @@
+package task3.controller;
+
+import org.apache.log4j.Logger;
+import task3.model.Book;
+import task3.views.MenuView;
+
+import java.io.*;
+import java.util.List;
+
+public class FileController {
+    private MenuView menuView;
+    private final Logger logger = Logger.getLogger(FileController.class);
+
+    public FileController(MenuView menuView) {
+        this.menuView = menuView;
+    }
+
+
+    boolean saveToFile(String address, List<Book> library) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(address))) {
+            for (Book book : library) {
+                out.writeObject(book);
+            }
+            out.writeObject(1);
+            logger.info("Saved to " + address);
+            return true;
+        } catch (IOException e) {
+            menuView.displayMessage(menuView.INVALID_PATH);
+            logger.error(menuView.INVALID_PATH);
+            return false;
+        }
+    }
+
+    List<Book> loadFile(String adress, List<Book> library) {
+        try {
+            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(adress))) {
+                for(Object book;(book= in.readObject()) instanceof Book;) {
+                    library.add((Book) book);
+                }
+                logger.info("File " + adress + " loaded");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            menuView.displayMessage(menuView.FILE_NOT_FOUND);
+            logger.error(menuView.FILE_NOT_FOUND);
+        }
+        return library;
+    }
+}
